1db5432c4c7f0ecdd8fc6fe4efa455be4c743a1c
Pranay Yerra <pranayhasan@gmail.com>
2025-03-06 14:06:40 +0530

    Init commit

---
+++ GoalAssistTests/GoalAssistTests.swift
@@ -0,0 +1,17 @@
+//
+//  GoalAssistTests.swift
+//  GoalAssistTests
+//
+//  Created by Pranay Hasan Yerra on 3/6/25.
+//
+
+import Testing
+@testable import GoalAssist
+
+struct GoalAssistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
